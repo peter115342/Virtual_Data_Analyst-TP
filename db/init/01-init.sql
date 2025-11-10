f8029a7c2f5f63d9ba8d0955565439b8e0bb8bcb
xkampova <xkampova@stuba.sk>
2025-11-10 10:57:38 +0100

    Add link to download CSV locally

--- db/init/01-init.sql
+++ db/init/01-init.sql
@@ -19,7 +19,8 @@ CREATE INDEX idx_product_id ON events(product_id);
 CREATE INDEX idx_user_id ON events(user_id);
 CREATE INDEX idx_category_code ON events(category_code);
 
--- Naplnenie dátami z CSV
+-- Download the CSV file from Kaggle and save it in db/init/ as 2019Nov_clean_small.csv
+-- Dataset from Kaggle: https://www.kaggle.com/datasets/mkechinov/ecommerce-behavior-data-from-multi-category-
 COPY events(
     event_time, event_type, product_id, category_id,
     category_code, brand, price, user_id, user_session
