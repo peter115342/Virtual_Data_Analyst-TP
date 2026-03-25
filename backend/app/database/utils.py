1090c4056cfe3d95f62eee5e8e7abdcd2a3018bf
JozefGulas <125763351+JozefGulas@users.noreply.github.com>
2026-03-25 23:11:13 +0100

    adad mssql-tools container, change - raw sql to sqlalchemy orm - copy from main, and the rest - idk, real mess, sql server not working

--- backend/app/database/utils.py
+++ backend/app/database/utils.py
@@ -1,28 +1,47 @@
 # connection urls
 
-def build_postgres_url(
+# def build_postgres_url(
+#         username: str,
+#         password: str,
+#         host: str,
+#         port: int,
+#         db_name: str,
+# ) -> str:
+#     return f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}"
+#
+# def build_mysql_url(
+#         username: str,
+#         password: str,
+#         host: str,
+#         port: int, # 3306
+#         db_name: str,
+# ) -> str:
+#     return f"mysql+pymysql://{username}:{password}@{host}:{port}/{db_name}"
+
+# def build_mssql_url(
+#         username: str,
+#         password: str,
+#         host: str,
+#         port: int,
+#         db_name: str,
+# ) -> str:
+#     return f"mssql+pyodbc://{username}:{password}@{host}:{port}/{db_name}?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
+
+
+def build_db_connection_url(
+        type: str,
         username: str,
         password: str,
         host: str,
         port: int,
         db_name: str,
 ) -> str:
-    return f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}"
-
-def build_mysql_url(
-        username: str,
-        password: str,
-        host: str,
-        port: int, # 3306
-        db_name: str,
-) -> str:
-    return f"mysql+pymysql://{username}:{password}@{host}:{port}/{db_name}"
-
-def build_mssql_url(
-        username: str,
-        password: str,
-        host: str,
-        port: int,
-        db_name: str,
-) -> str:
-    return f"mssql+pyodbc://{username}:{password}@{host}:{port}/{db_name}?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
+    if type == "postgres":
+        return f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}"
+    if type == "mysql":
+        return f"mysql+pymysql://{username}:{password}@{host}:{port}/{db_name}"
+    if type == "sqlserver":
+        return f"mssql+pyodbc://{username}:{password}@{host}:{port}/{db_name}?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes&ApplicationIntent=ReadOnly"
+    else:
+        # ZLE - OPRAVIT bud ENUM alebo neviem co
+        return "bad"
--- db/init_db_logistics/init/init.sh
+++ db/init_db_logistics/init/init.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-#SA_PASSWORD = ${MSSQL_SA_PASSWORD:-Strong_password123}
-
 echo " Waiting for SQL Server..."
-sleep 20
 
-echo " Creating database..."
-
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "
-IF DB_ID('LogisticsDB') IS NULL
-CREATE DATABASE LogisticsDB;
-"
+for i in {1..40}; do
+  /opt/mssql-tools/bin/sqlcmd -S db -U sa -P "$SA_PASSWORD" -Q "SELECT 1" > /dev/null 2>&1 && break
+  echo " SQL Server not ready yet..."
+  sleep 2
+done
 
 echo " Creating schema (tables without FK)..."
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d LogisticsDB -i /init/schema.sql
+/opt/mssql-tools/bin/sqlcmd -S db -U sa -P $SA_PASSWORD -i /init/schema.sql
 
 echo " Importing CSV data..."
 
@@ -23,15 +19,15 @@ do
   filename=$(basename "$file")
   tablename="${filename%.*}"
 
-  echo " Importing $tablename"
+  echo " Importing table: $tablename"
 
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d LogisticsDB -Q "
+  /opt/mssql-tools/bin/sqlcmd -S db -U sa -P $SA_PASSWORD -d LogisticsDB -Q "
   BULK INSERT [$tablename]
   FROM '/csv/$filename'
   WITH (
       FIRSTROW = 2,
       FIELDTERMINATOR = ',',
-      ROWTERMINATOR = '\n',
+      ROWTERMINATOR = '0x0a',
       TABLOCK,
       CODEPAGE = '65001'
   );
@@ -40,6 +36,6 @@ done
 
 echo " Adding foreign keys and constraints..."
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d LogisticsDB -i /init/constraints.sql
+/opt/mssql-tools/bin/sqlcmd -S db -U sa -P $SA_PASSWORD -d LogisticsDB -i /init/constraints.sql
 
 echo " DONE"
